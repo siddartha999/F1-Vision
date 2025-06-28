75d1b572abce146f3684ed942f143150ea5a6e08
Siddartha Tummala <siddarthatummala2@gmail.com>
2025-06-28 11:27:35 -0700

    Driver wins initial stab

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { DRIVER_WINS } from './data/driverWins';
 
 const App = () => {
 
@@ -10,6 +11,29 @@ const App = () => {
 
         <div id="body">
 
+          <div id="driver-wins" className='w-[40rem] bg-[rgb(32_33_36)] rounded-2xl p-2 m-20'>
+            <div id="header" className='ml-2 mt-1'>
+              <h3 className='font-semibold'>Top 10 Driver Wins</h3>
+            </div>
+            <div id="body" className='grid text-center gap-4 mt-4'>
+                <div id="driver-wins" className='grid grid-cols-6'>
+                  <p></p>
+                  <p className='col-span-3'>Driver</p>
+                  <p>Wins</p>
+                  <p>%</p>
+                </div>
+                {
+                  DRIVER_WINS.map((obj, index) => 
+                      <div className='grid grid-cols-6'>
+                        <p>{index + 1}</p>
+                        <p className='col-span-3'>{obj.driver}</p>
+                        <p>{obj.wins}</p>
+                        <p>{obj.percentage}</p>
+                    </div>
+                  )
+                }
+            </div>
+          </div>
         </div>
     </div>
   )
