0e65bd8bc507276f9a36e7412a2ecd828ec28d92
Siddartha Tummala <siddarthatummala2@gmail.com>
2025-06-28 12:03:30 -0700

    Top 10 Constructors By Wins initial stab

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,63 @@
 import './App.css';
+import { CONSTRUCTOR_WINS } from './data/constructorWins';
 import { DRIVER_WINS } from './data/driverWins';
 
 const App = () => {
 
   return (
-    <div id="main" className='bg-[rgb(21_21_22)] h-[inherit]'>
+    <div id="main" className='bg-[rgb(21_21_22)] h-[inherit] overflow-auto'>
         <div id="title" className='pt-6 text-center'>
           <p className='text-2xl text-white'>F1 Vision</p>
         </div>
 
         <div id="body">
-
-          <div id="driver-wins" className='w-[40rem] bg-[rgb(32_33_36)] rounded-2xl p-2 m-20'>
-            <div id="header" className='ml-2 mt-1'>
-              <h3 className='font-semibold'>Top 10 Driver Wins</h3>
-            </div>
-            <div id="body" className='grid text-center gap-4 mt-4'>
-                <div id="driver-wins" className='grid grid-cols-6'>
-                  <p></p>
-                  <p className='col-span-3'>Driver</p>
-                  <p>Wins</p>
-                  <p>%</p>
+          <div id="summary" className='flex flex-wrap justify-between'>
+               <div id="driver-wins" className='w-[35em] bg-[rgb(32_33_36)] rounded-2xl p-2 m-20'>
+                  <div id="header" className='ml-2 mt-1'>
+                    <h3 className='font-semibold'>Top 10 Drivers By Wins</h3>
+                  </div>
+                  <div id="body" className='grid text-center gap-4 mt-4'>
+                      <div id="driver-wins" className='grid grid-cols-6'>
+                        <p></p>
+                        <p className='col-span-3'>Driver</p>
+                        <p>Wins</p>
+                        <p>%</p>
+                      </div>
+                      {
+                        DRIVER_WINS.map((obj, index) => 
+                            <div className='grid grid-cols-6'>
+                              <p>{index + 1}</p>
+                              <p className='col-span-3 font-bold'>{obj.driver}</p>
+                              <p>{obj.wins}</p>
+                              <p>{obj.percentage}</p>
+                          </div>
+                        )
+                      }
+                  </div>
                 </div>
-                {
-                  DRIVER_WINS.map((obj, index) => 
-                      <div className='grid grid-cols-6'>
-                        <p>{index + 1}</p>
-                        <p className='col-span-3'>{obj.driver}</p>
-                        <p>{obj.wins}</p>
-                        <p>{obj.percentage}</p>
-                    </div>
-                  )
-                }
-            </div>
+                <div id="constructor-wins" className='w-[35rem] bg-[rgb(32_33_36)] rounded-2xl p-2 m-20'>
+                  <div id="header" className='ml-2 mt-1'>
+                    <h3 className='font-semibold'>Top 10 Constructors By Wins</h3>
+                  </div>
+                  <div id="body" className='grid text-center gap-4 mt-4'>
+                      <div id="constructor-wins" className='grid grid-cols-6'>
+                        <p></p>
+                        <p className='col-span-3'>Constructor</p>
+                        <p>Wins</p>
+                        <p>%</p>
+                      </div>
+                      {
+                        CONSTRUCTOR_WINS.map((obj, index) => 
+                            <div className='grid grid-cols-6'>
+                              <p>{index + 1}</p>
+                              <p className='col-span-3 font-bold'>{obj.constructor}</p>
+                              <p>{obj.wins}</p>
+                              <p>{obj.percentage}</p>
+                          </div>
+                        )
+                      }
+                  </div>
+                </div>
           </div>
         </div>
     </div>
---
+++ src/data/constructorWins.ts
@@ -0,0 +1,52 @@
+export const CONSTRUCTOR_WINS = [
+    {
+        "constructor": "Ferrari",
+        "wins": "248",
+        "percentage": "196"
+    },
+    {
+        "constructor": "Mclaren",
+        "wins": "196",
+        "percentage": "20.00"
+    },
+    {
+        "constructor": "Mercedes",
+        "wins": "130",
+        "percentage": "39.76"
+    },
+    {
+        "constructor": "Red Bull",
+        "wins": "124",
+        "percentage": "30.77"
+    },
+    {
+        "constructor": "Williams",
+        "wins": "114",
+        "percentage": "13.24"
+    },
+    {
+        "constructor": "Lotus",
+        "wins": "81",
+        "percentage": "13.37"
+    },
+    {
+        "constructor": "Brabham",
+        "wins": "35",
+        "percentage": "8.88"
+    },
+    {
+        "constructor": "Renault",
+        "wins": "35",
+        "percentage": "8.75"
+    },
+    {
+        "constructor": "Benetton",
+        "wins": "27",
+        "percentage": "10.38"
+    },
+    {
+        "constructor": "Tyrrell",
+        "wins": "23",
+        "percentage": "5.35"
+    },
+];
