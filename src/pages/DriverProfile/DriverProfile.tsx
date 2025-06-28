247a1e052d89d2a13739c9a605a3af357f05af4e
Siddartha Tummala <siddarthatummala2@gmail.com>
2025-06-28 16:44:32 -0700

    Driver profile enhancements

--- src/pages/DriverProfile/DriverProfile.tsx
+++ src/pages/DriverProfile/DriverProfile.tsx
@@ -1,7 +1,10 @@
 import { JSX } from "react";
 import { DRIVERS } from "../../data/drivers";
 import DriverSummary from "./DriverSummary";
-
+import LewisHamiltonImage from "../../assets/drivers/lewis-hamilton.png";
+import FerrariImage from "../../assets/constructors/Ferrari.png";
+import MercedesImage from "../../assets/constructors/Mercedes.png";
+import MclarenImage from "../../assets/constructors/Mclaren.png";
 
 /**
  * Driver profile page.
@@ -10,21 +13,27 @@ const DriverProfile = (): JSX.Element => {
     const driver = DRIVERS[0];
     return (
         <div id="driver-profile">
-            <div id="intro" className="relative left-[20%] w-[80%] lg:w-[60%] h-[6rem] rounded-2xl p-2 mt-10 bg-ferrari-bg-theme">
-                <div id="image"></div>
-                <div id="content" className="self-baseline mt-4">
+            <div id="intro" className="relative left-[20%] w-[80%] lg:w-[60%] h-[10rem] rounded-2xl p-2 mt-10 bg-ferrari-bg-theme flex gap-4">
+                <div id="image" className="h-[2rem]">
+                    <img src={LewisHamiltonImage} className="object-contain h-[8rem]" />
+                </div>
+                <div id="content" className=" grid grid-rows-2">
                     <div id="name-section" className="flex flex-wrap gap-3 items-baseline">
                         <p id="name" className="text-2xl font-bold">{driver.firstName} {driver.lastName}</p>
                         <div className="rounded-2xl px-3 py-0.75 bg-white cursor-pointer">
                             <p id="team" className="text-ferrari-bg-theme">{driver.currentTeam}</p>
                         </div>
                     </div>
+                     <div id="team-images" className="flex flex-wrap gap-2 align-center self-end">
+                        <img src={FerrariImage} className="object-contain h-[6rem]"/>
+                        <img src={MercedesImage} className="object-contain h-[6rem] w-[4rem]" />
+                        <img src={MclarenImage} className="object-contain h-[6rem]"/>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <DriverSummary />
-            </div>
+            <DriverSummary />
+            
         </div>
     );
 };
--- src/pages/DriverProfile/DriverSummary.tsx
+++ src/pages/DriverProfile/DriverSummary.tsx
@@ -3,9 +3,9 @@ import { DRIVERS } from "../../data/drivers";
 const DriverSummary = () => {
     const driver = DRIVERS[0];
     return (
-        <div id="driver-summary">
-            <div className="relative left-[20%] w-[20rem] lg:w-[30rem] rounded-2xl p-2 mt-10 bg-ferrari-bg-theme">
-                <div className="grid grid-rows-2 grid-cols-3 gap-6 justify-items-center">
+        <div id="driver-summary" className="relative left-[20%] flex flex-wrap gap-4 lg:justify-between lg:w-[60%]">
+            <div id="career-summary" className="w-[20rem] lg:w-[30rem] rounded-2xl p-2 mt-10 bg-ferrari-bg-theme">
+                <div className="grid grid-rows-2 grid-cols-3 gap-6 justify-items-center text-center">
                     <div id="number">
                         <p className="text-xl font-semibold">{driver.number}</p>
                         <p className="mt-1">Number</p>
@@ -13,6 +13,10 @@ const DriverSummary = () => {
                     <div id="birthdate">
                         <p className="text-xl font-semibold">{driver.dob}</p>
                         <p className="mt-1">Birthdate</p>
+                    </div>
+                    <div id="nationality">
+                        <p className="text-xl font-semibold">{driver.nationality}</p>
+                        <p className="mt-1">Nationality</p>
                     </div>
                     <div id="height">
                         <p className="text-xl font-semibold">{driver.height}</p>
@@ -26,16 +30,38 @@ const DriverSummary = () => {
                         <p className="text-xl font-semibold">{driver.constructorTitles}</p>
                         <p className="mt-1">WCC</p>
                     </div>
+                </div>
+            </div>
+        
+            <div id="race-summary" className="w-[20rem] lg:w-[30rem] rounded-2xl p-2 mt-10 bg-ferrari-bg-theme">
+                <div className="grid grid-rows-2 grid-cols-3 gap-6 justify-items-center text-center">
+                    <div id="races">
+                        <p className="text-xl font-semibold">{driver.grandPrixStats.races}</p>
+                        <p className="mt-1">Races</p>
+                    </div>
                     <div id="podiums">
-                        <p className="text-xl font-semibold">{driver.podiums}</p>
+                        <p className="text-xl font-semibold">{driver.grandPrixStats.wins}</p>
+                        <p className="mt-1">Wins</p>
+                    </div>
+                    <div id="poles">
+                        <p className="text-xl font-semibold">{driver.grandPrixStats.poles}</p>
+                        <p className="mt-1">Poles</p>
+                    </div>
+                    <div id="podiums">
+                        <p className="text-xl font-semibold">{driver.grandPrixStats.podiums}</p>
                         <p className="mt-1">Podiums</p>
+                    </div>
+                    <div id="points">
+                        <p className="text-xl font-semibold">{driver.points}</p>
+                        <p className="mt-1">Points</p>
+                    </div>
+                    <div id="fastest-laps">
+                        <p className="text-xl font-semibold">{driver.grandPrixStats.fastestLaps}</p>
+                        <p className="mt-1">Fastest Laps</p>
                     </div>
                 </div>
             </div>
 
-            <div>
-
-            </div>
         </div>
     );
 };
