1383d3edd44ad99a2504fc5c1773cba844d71c3c
Siddartha Tummala <siddarthatummala2@gmail.com>
2025-06-28 10:40:14 -0700

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div id="main" className='bg-[rgb(21_21_22)] h-[inherit]'>
+        <div id="title" className='pt-6 text-center'>
+          <p className='text-2xl text-white'>F1 Vision</p>
+        </div>
+
+        <div id="body">
+
+        </div>
+    </div>
+  )
+};
+
+export default App;
